a5b5fccd1d409a4eef540ec51db0ada69acf1991
rubairshaid <rubairshed1@gmail.com>
2019-11-03 01:13:38 +0200

    changes

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/*let board = document.getElementById("Board");
+//var click = document.getElementsByClassName("newBoard");
+let boards =new Array ();
+
+function createBoard()
+{
+
+}
+click.onclick = function (){
+    
+}*/
+
+/*let cnt =0 ; 
+
+function createBoard (){
+    var newboard = document.createElement('button');
+    var r= document.getElementsById('boardList')[0];
+    r.appendChild(newboard);
+
+}*/
+
+function openB(ev , Bname){
+    var tabcontent = document.getElementsByClassName("Bcontent");
+    for(let i=0;i<tabcontent.length;i++)
+    tabcontent[i].style.display= "none";
+    var tabName=document.getElementsByClassName("tab");
+    for(let i=0;i<tabcontent.length;i++)
+    tabName[i].classname = tabName[i].className.replace("active","");
+    document.getElementById(Bname).style.display = "block";
+    ev.currentTarget.className += " active";
+}
